866e1cd118a600ff58542275573df570047d7740
Brandi Hornbuckle <brandi.hornbuckle@gmail.com>
2024-07-24 19:57:36 -0400

    Added classes for BlogPost, CommentPost, and User tables and got the MudBlazor table functioning, but without data yet.

---
+++ EmrysSerenShared/User.cs
@@ -0,0 +1,10 @@
+﻿namespace EmrysSerenShared
+{
+    public class User
+    {
+        public int UserID { get; set; }
+        public string UserName { get; set; }
+        public string UserEmail { get; set; }
+        public byte[] UserAvatar { get; set; }
+    }
+}
