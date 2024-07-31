feb4becc147a05ec2d06c00fa953c94e686cb873
Brandi Hornbuckle <brandi.hornbuckle@gmail.com>
2024-07-31 18:36:23 -0400

    Setting up database connections in preparation for running migrations.

--- EmrysSeren2.0/Program.cs
+++ EmrysSeren2.0/Program.cs
@@ -1,10 +1,15 @@
 ﻿using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using EmrysSeren2;
+using EmrysSerenData;
+using Microsoft.Extensions.Configuration;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
+var connectionString = builder.Configuration.GetConnectionString("BlogDB");
+
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
--- EmrysSerenData/ESDbContext.cs
+++ EmrysSerenData/ESDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using EmrysSerenShared;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace EmrysSerenAPI.Models
 {
@@ -13,6 +14,18 @@ namespace EmrysSerenAPI.Models
         public DbSet<BlogPost> BlogPosts { get; set; }  
         public DbSet<CommentPost> CommentPosts { get; set; }
         public DbSet<User> Users { get; set; }
+
+        protected readonly IConfiguration Configuration;
+
+        public ESDbContext(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(Configuration.GetConnectionString(BlogDB));
+        }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
